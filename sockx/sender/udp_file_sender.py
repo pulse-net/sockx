587b32c7624d8fa5c41adb1be67a5d9ce06b7c3f
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-08-09 00:18:32 +0530

    Update host -> receiver_ip

--- sockx/sender/udp_file_sender.py
+++ sockx/sender/udp_file_sender.py
@@ -9,8 +9,8 @@ from ..constants import *
 
 
 class UDPFileSender:
-    def __init__(self, host, port=PORT):
-        self.__host = host
+    def __init__(self, receiver_ip, port=PORT):
+        self.__receiver_ip = receiver_ip
         self.__port = port
 
     def send_file(self, filename):
@@ -21,7 +21,7 @@ class UDPFileSender:
         s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
         # Address to send to
-        addr = (self.__host, self.__port)
+        addr = (self.__receiver_ip, self.__port)
 
         # Send the filename and filesize
         s.sendto(f"{filename}{SEPARATOR}{filesize}".encode(), addr)
